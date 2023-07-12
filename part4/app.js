58cfcb6ae8cf25ff2ed1bbcafa3201613593768d
symatt <jamesxsy17@gmail.com>
2023-07-12 14:09:57 +0800

    refactored project structure for 4.2

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const blogsRouter = require('./controllers/blogs');
+
+mongoose
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB');
+	})
+	.catch((error) => {
+		logger.error('error connecting to MongoDB: ', error.message);
+	});
+
+app.use(cors());
+// app.use(express.static('dist'))
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
