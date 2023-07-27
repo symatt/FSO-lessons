bf1aecded74bb42d7079c0be6a1b5b45f126d9ab
symatt <jamesxsy17@gmail.com>
2023-07-27 16:46:57 +0800

    updated with part 5 testing react apps

--- part2/src/components/NoteForm.jsx
+++ part2/src/components/NoteForm.jsx
@@ -14,13 +14,14 @@ const NoteForm = ({ createNote }) => {
 	};
 
 	return (
-		<div>
+		<div className='formDiv'>
 			<h2>Create a new note</h2>
 
 			<form onSubmit={addNote}>
 				<input
 					value={newNote}
 					onChange={(event) => setNewNote(event.target.value)}
+					placeholder='write note content here'
 				/>
 				<button type='submit'>save</button>
 			</form>
